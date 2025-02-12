5688fa93da1a484da51b5d05596d8c8090abc0b3
Dylan Davies <dylandaviddavies@gmail.com>
2025-02-12 04:31:24 -0500

    update vite

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,8 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://dylandaviddavies.github.io/untap",
+        origin: "https://dylandaviddavies.github.io",
+        base: "/untap/"
       }),
     ],
   };
